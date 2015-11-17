2db40e3fb980956cc1b9e7fa29a27ebd3c6b5747
Davor Obilinovic <Obee@Davors-MacBook-Pro.local>
2015-11-17 13:00:16 +0100

    hiding panels in market

--- src/main/java/custom/components/panels/Market/MarketCardView.java
+++ src/main/java/custom/components/panels/Market/MarketCardView.java
@@ -1,13 +1,19 @@
 package custom.components.panels.Market;
 
+import com.mongodb.DBObject;
 import custom.classes.Market.MarketCard;
 import custom.components.ImageWindow;
 import obee.pages.MarketPage;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+
+import java.util.List;
 
 
 /**
@@ -19,13 +25,14 @@ public class MarketCardView extends Panel {
     private final int jadAvailableForBidding;
     private ImageWindow view;
     private final String userName;
-    private Label statusLbl;
+    private Label biddersNumLbl, hatersNumLbl;
     private Label priceLbl;
     private final int price;
     private Label hatersCntLbl;
     private Form bidForm, hateForm;
     private Label timeLbl;
     private Label nameLbl;
+    private ListView bidsList, hatersList;
 
     public MarketCardView(String id, final MarketCard card, final String userName, int jadAvailableForBidding) {
         super(id);
@@ -85,9 +92,11 @@ public class MarketCardView extends Panel {
         this.timeLbl = new Label("timeLbl",card.getTimeToLooseString());
         add(timeLbl);
 
-        String status = card.listHaters().contains(userName)? "You hated this card!" : card.bidingStatus();
-        this.statusLbl = new Label("statusLbl", status);
-        add(statusLbl);
+        this.biddersNumLbl = new Label("biddersNumLbl", card.bids.size()+" bids");
+        add(biddersNumLbl);
+
+        this.hatersNumLbl= new Label("hatersNumLbl", card.listHaters().size()+" hates");
+        add(hatersNumLbl);
 
         this.hatersCntLbl = new Label("hatersCntLbl", "H:"+card.getHatersCnt());
         add(hatersCntLbl);
@@ -98,5 +107,25 @@ public class MarketCardView extends Panel {
         this.view = new ImageWindow("view", new Model<String>(card.getImageUrl().toString()), null);
         this.view.setUrl(card.getImageUrl());
         add(view);
+
+        this.bidsList = new ListView("bidsList", new PropertyModel<List>(this, "card.bids")) {
+
+            @Override
+            protected void populateItem(ListItem item) {
+                DBObject bid = (DBObject) item.getModelObject();
+                item.add(new Label("bidLbl", bid.get("userName").toString()+" - "+bid.get("value").toString()));
+            }
+        };
+        add(bidsList);
+
+        this.hatersList = new ListView("hatersList", new PropertyModel<List>(this, "card.haters")) {
+
+            @Override
+            protected void populateItem(ListItem item) {
+                String hater = item.getModelObject().toString();
+                item.add(new Label("haterLbl", hater));
+            }
+        };
+        add(hatersList);
     }
 }
